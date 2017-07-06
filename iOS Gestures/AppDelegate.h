cca2714df88192fb5fe29bf8c3fa92be7941091b
Rob Wymer <wymer1120@gmail.com>
2017-07-06 12:59:53 -0400

    First Commit

---
+++ iOS Gestures/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS Gestures
+//
+//  Created by Alex Wymer  on 2017-07-06.
+//  Copyright © 2017 Sav Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
